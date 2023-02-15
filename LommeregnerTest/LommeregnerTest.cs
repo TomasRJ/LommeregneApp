b97eabf11fbc70aea68d83f154df051baaa31a94
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-02-15 15:22:04 +0100

    Rettede op på LommeregnerTest koden

--- LommeregnerTest/LommeregnerTest.cs
+++ LommeregnerTest/LommeregnerTest.cs
@@ -1,39 +1,32 @@
-﻿using AngleSharp.Dom;
-using AngleSharp.Html.Dom;
-using Bunit;
+﻿using Bunit;
 using LommeregneApp.Pages;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
 
 namespace LommeregnerTest
 {
     public class LommeregnerTest : TestContext
     {
         [Fact]
-        public void TestCounter()
+        public void AddTest()
         {
             // Arrange
-            var counterComponent = RenderComponent<Lommeregner>();
+            var calcComponent = RenderComponent<Lommeregner>();
 
-            counterComponent.Find("#num1").MarkupMatches("<input id=\"num1\" placeholder=\"Enter First Number\" >");
-            counterComponent.Find("#num2").MarkupMatches("<input id=\"num2\" placeholder=\"Enter Second Number\" >");
-            counterComponent.Find("#finalresult").MarkupMatches("<input id=\"finalresult\" readonly >");
+            calcComponent.Find("#num1").MarkupMatches("<input id=\"num1\" placeholder=\"Enter First Number\" >");
+            calcComponent.Find("#num2").MarkupMatches("<input id=\"num2\" placeholder=\"Enter Second Number\" >");
+            calcComponent.Find("#finalresult").MarkupMatches("<input id=\"finalresult\" readonly >");
 
-            var num1 = counterComponent.Find("#num1");
-            var num2 = counterComponent.Find("#num2");
-            var finalresult = counterComponent.Find("#finalresult");
-            var addButton = counterComponent.Find("#AddBtn");
+            var num1 = calcComponent.Find("#num1");
+            var num2 = calcComponent.Find("#num2");
+            var finalresult = calcComponent.Find("#finalresult");
+            var addButton = calcComponent.Find("#AddBtn");
 
 
             // Act
-            num1.Change("2");          
+            num1.Change("2");
             num2.Change("3");
             addButton.Click();
 
-            //Asser
-
+            // Assert
             Assert.Equal("5", finalresult.GetAttribute("value"));
         }
     }
