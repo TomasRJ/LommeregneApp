03946afa026964814b394046f76897d41c4936db
Sielth <sielth@hotmail.it>
2023-02-16 06:47:48 +0100

    merge conflict solved

--- LommeregnerTest/LommeregnerTest.cs
+++ LommeregnerTest/LommeregnerTest.cs
@@ -15,25 +15,25 @@ namespace LommeregnerTest
     {
       // Arrange
       Services.AddSingleton(typeof(IAdvancedOperations), _advancedOperations);
-      var counterComponent = RenderComponent<Lommeregner>();
+      var calcComponent = RenderComponent<Lommeregner>();
 
-            calcComponent.Find("#num1").MarkupMatches("<input id=\"num1\" placeholder=\"Enter First Number\" >");
-            calcComponent.Find("#num2").MarkupMatches("<input id=\"num2\" placeholder=\"Enter Second Number\" >");
-            calcComponent.Find("#finalresult").MarkupMatches("<input id=\"finalresult\" readonly >");
+      calcComponent.Find("#num1").MarkupMatches("<input id=\"num1\" placeholder=\"Enter First Number\" >");
+      calcComponent.Find("#num2").MarkupMatches("<input id=\"num2\" placeholder=\"Enter Second Number\" >");
+      calcComponent.Find("#finalresult").MarkupMatches("<input id=\"finalresult\" readonly >");
 
-            var num1 = calcComponent.Find("#num1");
-            var num2 = calcComponent.Find("#num2");
-            var finalresult = calcComponent.Find("#finalresult");
-            var addButton = calcComponent.Find("#AddBtn");
+      var num1 = calcComponent.Find("#num1");
+      var num2 = calcComponent.Find("#num2");
+      var finalresult = calcComponent.Find("#finalresult");
+      var addButton = calcComponent.Find("#AddBtn");
 
 
-            // Act
-            num1.Change("2");
-            num2.Change("3");
-            addButton.Click();
+      // Act
+      num1.Change("2");
+      num2.Change("3");
+      addButton.Click();
 
-            // Assert
-            Assert.Equal("5", finalresult.GetAttribute("value"));
-        }
+      // Assert
+      Assert.Equal("5", finalresult.GetAttribute("value"));
     }
+  }
 }
