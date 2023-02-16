b4c65252c93a2d5638f98333f36a5f7e360f8975
Sielth <sielth@hotmail.it>
2023-02-16 06:53:15 +0100

    merged branches + added missing dependencies into divide and multiply

--- LommeregnerTest/MultiplyTest/MultiplyTest.cs
+++ LommeregnerTest/MultiplyTest/MultiplyTest.cs
@@ -1,32 +1,39 @@
 ﻿using Bunit;
+using LommeregneApp.Features;
 using LommeregneApp.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 
 namespace LommeregnerTest.MultiplyTest
 {
-    public class MultiplyTest : TestContext
+  public class MultiplyTest : TestContext
+  {
+    private readonly IAdvancedOperations _advancedOperations = Substitute.For<IAdvancedOperations>();
+
+    [Fact]
+    public void Test()
     {
-        [Fact]
-        public void Test()
-        {
-            // Arrange
-            var calcComponent = RenderComponent<Lommeregner>();
+      // Arrange
+      Services.AddSingleton(typeof(IAdvancedOperations), _advancedOperations);
 
-            calcComponent.Find("#num1").MarkupMatches("<input id=\"num1\" placeholder=\"Enter First Number\" >");
-            calcComponent.Find("#num2").MarkupMatches("<input id=\"num2\" placeholder=\"Enter Second Number\" >");
-            calcComponent.Find("#finalresult").MarkupMatches("<input id=\"finalresult\" readonly >");
+      var calcComponent = RenderComponent<Lommeregner>();
 
-            var num1 = calcComponent.Find("#num1");
-            var num2 = calcComponent.Find("#num2");
-            var finalresult = calcComponent.Find("#finalresult");
-            var mulButton = calcComponent.Find("#MulBtn");
+      calcComponent.Find("#num1").MarkupMatches("<input id=\"num1\" placeholder=\"Enter First Number\" >");
+      calcComponent.Find("#num2").MarkupMatches("<input id=\"num2\" placeholder=\"Enter Second Number\" >");
+      calcComponent.Find("#finalresult").MarkupMatches("<input id=\"finalresult\" readonly >");
 
-            // Act
-            num1.Change("7");
-            num2.Change("191");
-            mulButton.Click();
+      var num1 = calcComponent.Find("#num1");
+      var num2 = calcComponent.Find("#num2");
+      var finalresult = calcComponent.Find("#finalresult");
+      var mulButton = calcComponent.Find("#MulBtn");
 
-            // Assert
-            Assert.Equal("1337", finalresult.GetAttribute("value"));
-        }
+      // Act
+      num1.Change("7");
+      num2.Change("191");
+      mulButton.Click();
+
+      // Assert
+      Assert.Equal("1337", finalresult.GetAttribute("value"));
     }
+  }
 }
